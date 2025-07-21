58aa84bb4ebff44d27ee4278faaae9495a32d6c5
Ivan Art <tobedrive@yandex.ru>
2025-07-21 12:47:50 +0300

    добавил немного кода для запуска бота + файл .env

--- app/main.py
+++ app/main.py
@@ -1,3 +1,25 @@
-#@MasterManifest_Bot
-cjzncjnzckl
-аываывы
+import asyncio
+import os
+
+
+from aiogram import Bot, Dispatcher, types, F
+
+
+
+
+bot = Bot(token=os.getenv("TOKEN"), default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+dp = Dispatcher()
+
+
+
+
+
+async def main():
+        await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("Exit")
